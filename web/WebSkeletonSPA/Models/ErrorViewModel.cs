3a107a4da97c40a71fd37e3a511d6909d1971413
Trevor Frewin <tjfrewin@gmail.com>
2024-10-18 10:57:41 +0100

    Basic SPA web app foundations only.

---
+++ web/WebSkeletonSPA/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebSkeletonSPA.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
